8a2b124c5a2aeda50d1738a935d66fc980a24ae7
Omar Iraqi <o.iraqi@gmail.com>
2019-05-04 17:19:35 +0100

    restructure

---
+++ customer/progress/src/main/java/ma/aui/sse/it/xcommerce/microservices/customer/XcommerceMicroservicesCustomerApplication.java
@@ -0,0 +1,13 @@
+package ma.aui.sse.it.xcommerce.microservices.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XcommerceMicroservicesCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XcommerceMicroservicesCustomerApplication.class, args);
+	}
+
+}
---
+++ order/progress/src/main/java/ma/aui/sse/it/xcommerce/microservices/order/XcommerceMicroservicesOrderApplication.java
@@ -0,0 +1,13 @@
+package ma.aui.sse.it.xcommerce.microservices.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XcommerceMicroservicesOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XcommerceMicroservicesOrderApplication.class, args);
+	}
+
+}
